06cd40377826229538800c8e767e1dbd4d9b43c0
sinlov <sinlovgmppt@gmail.com>
2023-07-23 20:12:39 +0800

    feat: dist action-standard-verison

--- dist/index.js
+++ dist/index.js
@@ -16,8 +16,9 @@ async function run() {
   try {
     // let configuration = getConfiguration();
     // configuration.dryRun = true
+    let tagPrefix = core.getInput('tag-prefix');
     let releaseAs = core.getInput('release-as');
-    const releaseReg =  core.getInput('release-ref-regexp')
+    const releaseReg = core.getInput('release-ref-regexp')
     if (!releaseReg || releaseReg === '') {
       core.setFailed('release-ref-regexp is empty');
       return;
@@ -26,22 +27,25 @@ async function run() {
     if (releaseAs === '') {
       let releaseByRef = core.getInput('release-by-ref');
       if (!releaseByRef || releaseByRef === '') {
-        core.error('release-by-ref is empty');
-        core.setFailed("'release-by-ref is empty'")
+        core.error(`release-by-ref is empty`);
+        core.setFailed(`release-by-ref is empty`)
         return;
       }
       core.info(`release-by-ref: ${releaseByRef}`);
       const match = releaseByRef.match(releaseReg);
       if (!match) {
         core.error('release-by-ref not match release-ref-regexp');
-        core.ExitCode = 2;
+        core.setFailed(`release-by-ref not match release-ref-regexp ${releaseReg} from ${releaseByRef}`)
         return;
       }
       releaseAs = match[1].trim()
     }
 
     if (core.getInput('dry-run') === 'true') {
-      cli.parse(`standard-version --dry-run --release-as ${releaseAs}`);
+      cli.parse(`standard-version --dry-run --tag-prefix '${tagPrefix}' --release-as ${releaseAs}`);
+      await standardVersion(cli.argv);
+    } else {
+      cli.parse(`standard-version --tag-prefix '${tagPrefix}' --release-as ${releaseAs}`);
       await standardVersion(cli.argv);
     }
 
